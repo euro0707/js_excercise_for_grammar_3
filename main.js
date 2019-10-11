746f8a9833943ec6ee950d281d09023a28d4c097
Tetsuya <52840003+euro0707@users.noreply.github.com>
2019-10-11 21:07:02 +0900

    kadaisyuusei2

--- main.js
+++ main.js
@@ -27,11 +27,10 @@ console.log(num);
 //       - answerとinputの値が同じだったら「正解です！」とalertで表示する
 //     - 正解が出るまでpromptで問題文を出し続ける
 //     - 正解が出たらWhileループを抜ける
-
 const question = "パンはパンでも食べられないパンは？";
 const answer = "フライパン";
 let input = "";
-while (input != answer) {
+while (input !== answer) {
     input = prompt(question);
     if (input === answer) {
         alert('正解です！');
